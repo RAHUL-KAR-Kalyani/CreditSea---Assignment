7f93335f09bbec706508c1d8265f73f5d678a50c
Rahul Kar <108023178+RAHUL-KAR-Kalyani@users.noreply.github.com>
2025-10-18 18:27:54 +0530

    cors api updated

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,8 @@ app.use(helmet());
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173'
+    origin: "https://creditsea-assignment-adno.onrender.com",
+    credentials: true
 }));
 
 app.use('/api', uploadRouter);
